cbb7b1f40934cd4cb4b2b45219a1de0c620170ee
zengjie <zjengjie@gmail.com>
2021-04-06 10:22:16 +0800

    [88] Merge Sorted Array

---
+++ algorithms/88.merge-sorted-array.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=88 lang=java
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = m + n - 1;
+        m--;
+        n--;
+        while(n >= 0) {
+            if (m >= 0 && nums1[m] > nums2[n]) {
+                nums1[index] = nums1[m];
+                m--;
+            } else {
+                nums1[index] = nums2[n];
+                n--;
+            }
+            index--;
+        }
+    }
+}
+// @lc code=end
+
