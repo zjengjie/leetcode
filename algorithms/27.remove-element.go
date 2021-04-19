1f42ec9453ea8e1401fdd797e380ab3f70eb5916
zengjie <zjengjie@gmail.com>
2021-04-19 20:17:52 +0800

    [27] Remove Element

---
+++ algorithms/27.remove-element.go
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=27 lang=golang
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+func removeElement(nums []int, val int) int {
+	index := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[index] = nums[i]
+			index++
+		}
+	}
+	return index
+}
+
+// @lc code=end
+
